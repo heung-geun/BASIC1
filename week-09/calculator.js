70eb07e91bcdf4bc385b66846bf60bbb08d4151e
Hyeunggeun-Yun <gudrms121712@naver.com>
2025-02-06 11:51:43 +0900

    커밋 뭔지모름...

---
+++ week-09/calculator.js
@@ -0,0 +1,19 @@
+// calculator.js
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const subtract = (a, b) => {
+  return a - b;
+};
+
+export const multiply = (a, b) => {
+  return a * b;
+};
+
+export const divide = (a, b) => {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
+};
---
+++ week-09/calculator.test.js
@@ -0,0 +1,3 @@
+test("contain 테스트", () => {
+  expect("HelloWorld").toContain(" ");
+});
